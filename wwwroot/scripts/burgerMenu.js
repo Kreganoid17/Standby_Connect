8d45ec03be69caa6ef61413ea9a3933d19415bfd
KubashenC <KubashenC@ZAZN-UMH-BBC255>
2023-11-13 13:27:39 +0200

    added home page elements

---
+++ wwwroot/scripts/burgerMenu.js
@@ -0,0 +1,11 @@
+﻿function openNav() {
+    document.getElementById("sideNavBar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("sideNavBar").style.padding = "15px 20px 15px 20px";
+}
+
+function closeNav() {
+    document.getElementById("sideNavBar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("sideNavBar").style.padding = "0px";
+}
---
+++ wwwroot/scripts/homepage.js
@@ -0,0 +1,26 @@
+﻿const delay = 2000;
+let calltimer;
+let isHoldingCallButton;
+
+function call() {
+
+    isHoldingCallButton = true;
+    calltimer = setTimeout(() => {
+
+        //a trigger to start a call
+        console.log("call started....");
+    }, delay);
+
+}
+
+
+window.addEventListener('mouseup', (e) => {
+
+    if (isHoldingCallButton) {
+        if (calltimer) {
+
+            clearTimeout(calltimer);
+        }
+    }
+
+});
